91cfde4549f29bd930740d0e1afec56a7d84382b
Hekwang Lhi <hekwang.lhi@gmail.com>
2022-02-16 17:13:08 -0500

    github url

--- src/component/pages/Footer.js
+++ src/component/pages/Footer.js
@@ -3,11 +3,15 @@ import { AiOutlineGithub } from "react-icons/ai";
 import { GrLinkedin, GrTwitter } from "react-icons/gr";
 
 export default function Footer() {
+  const handleClick = () => {
+    window.open("https://github.com/klhi3?tab=repositories");
+  }
+
   return (
     <div className="container"> 
         <div className="d-flex justify-content-center">
         <p>
-            <button type="button" className="btn" onclick="window.open('https://github.com/klhi3?tab=repositories')"> <AiOutlineGithub /></button> 
+            <button type="button" className="btn" onClick={handleClick}> <AiOutlineGithub /></button> 
             <button type="button" className="btn"> <GrLinkedin /></button>
             <button type="button" className="btn"> <GrTwitter /></button>
             
